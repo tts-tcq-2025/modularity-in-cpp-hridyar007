40a846aa9850acdcf68a4c9d6bdbb437f2eba567
hridyar007 <hridyar007@gmail.com>
2025-08-12 09:30:39 +0530

    Create Constants.cpp

---
+++ Constants.cpp
@@ -0,0 +1,10 @@
+#include "Constants.h"
+
+namespace TelCoColorCoder
+{
+    const char* MajorColorNames[] = {"White", "Red", "Black", "Yellow", "Violet"};
+    const char* MinorColorNames[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
+
+    const int numberOfMajorColors = sizeof(MajorColorNames) / sizeof(MajorColorNames[0]);
+    const int numberOfMinorColors = sizeof(MinorColorNames) / sizeof(MinorColorNames[0]);
+}
